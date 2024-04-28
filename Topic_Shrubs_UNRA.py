e104fe88c09f291c763775cfbe0d09135b71139c
RangNu <unrapoland@gmail.com>
2024-04-28 23:30:14 +0200

    uploading file

---
+++ Topic_Shrubs_UNRA.py
@@ -0,0 +1,10 @@
+#UN RA / group 4
+class Plant :
+    def Plant_Information(self):
+        self.name = ""
+        self.size = ""
+        self.height = ""
+        self.width = ""
+        self.color = ""
+
+
