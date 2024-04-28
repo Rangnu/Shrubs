7af2c8ca53427009632e65ca2dcf07cba7925564
RangNu <unrapoland@gmail.com>
2024-04-29 01:18:40 +0200

    adding args

--- Topic_Shrubs_UNRA.py
+++ Topic_Shrubs_UNRA.py
@@ -1,10 +1,17 @@
 #UN RA / group 4
-class Plant :
-    def Plant_Information(self):
-        self.name = ""
-        self.size = ""
-        self.height = ""
-        self.width = ""
-        self.color = ""
+class Shrub :
+    def __init__(self, name, species, height, width, color, climate):
+        self.name = name
+        self.species = species
+        self.height = height
+        self.width = width
+        self.color = color
+        self.climate = climate
 
-
+    def display_info(self):
+        print(f"Name : {self.name}")
+        print(f"Species : {self.species}")
+        print(f"Height : {self.height}")
+        print(f"Width : {self.width}")
+        print(f"Color : {self.color}")
+        print(f"Climate : {self.climate}")
