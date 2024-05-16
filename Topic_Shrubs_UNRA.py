f739f6d25546beb94171678672420fa00f411b5d
RangNu <unrapoland@gmail.com>
2024-05-16 15:52:44 +0200

    adding some

--- Topic_Shrubs_UNRA.py
+++ Topic_Shrubs_UNRA.py
@@ -1,17 +1,26 @@
 #UN RA / group 4
 class Shrub :
-    def __init__(self, name, species, height, width, color, climate):
+    def __init__(self, name, species, height, width, color, climate, average_lifespan, essential_tool):
         self.name = name
         self.species = species
         self.height = height
         self.width = width
         self.color = color
         self.climate = climate
+        self.average_lifespan = average_lifespan
+        self.essential_tool = essential_tool
+        
 
     def display_info(self):
-        print(f"Name : {self.name}")
-        print(f"Species : {self.species}")
-        print(f"Height : {self.height}")
-        print(f"Width : {self.width}")
-        print(f"Color : {self.color}")
-        print(f"Climate : {self.climate}")
+        info = [
+            f"Name : {self.name}",
+            f"Species : {self.species}",
+            f"Height : {self.height}",
+            f"Width : {self.width}",
+            f"Color : {self.color}",
+            f"Climate : {self.climate}",
+            f"Average Lifespan : {self.average_lifespan}",
+            f"Essential Tool : {self.essential_tool}"
+       ]
+        
+    print("\n".join(info))
